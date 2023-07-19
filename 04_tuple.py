5a149fd85e9788e7f6987db10b251d9c2e5ce2fa
AhsenRiaz <ahsenriaz2001@gmail.com>
2023-07-19 21:56:36 +0500

    Create 04_tuple.py

---
+++ 04_tuple.py
@@ -0,0 +1,13 @@
+
+# Tuples are used to store multiple items in a single variable
+# Tuples are immutable
+# Tuples have static characteristics
+my_tuple = ("Machine Learning", 2, 3.14);
+print(my_tuple);
+
+# converting list to tuple
+
+cars = ["mercedes", "corolla",];
+
+cars_to_typle = tuple(cars);
+print(cars_to_typle);
