4bfe92aa0ebc8314c3a652a9d5a102099c51201b
AhsenRiaz <ahsenriaz2001@gmail.com>
2023-07-19 21:57:03 +0500

    Create 07_conditional_statements.py

---
+++ 07_conditional_statements.py
@@ -0,0 +1,13 @@
+is_eligible = int(input('What is your age? ')) ;
+
+if is_eligible < 18:
+    print('not eligible')
+
+elif is_eligible == 18:
+    print('eligible')
+
+else:
+    print('over qualified')
+
+
+
