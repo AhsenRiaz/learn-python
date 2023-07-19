4ea96d876cc667faae6bc9885823ee2c8c54b6c2
AhsenRiaz <ahsenriaz2001@gmail.com>
2023-07-19 21:56:08 +0500

    Create 01_variables.py

---
+++ 01_variables.py
@@ -0,0 +1,20 @@
+#  String 
+name = "Bob";
+surname = 'John'
+
+full_name = name.capitalize() + ' ' + surname.capitalize();
+
+print("fullname:", full_name);
+
+print(len(name));
+print(len(surname));
+
+# Numbers
+age = 18;
+
+pi = 3.4;
+
+cars = ['bmw', 'mercedes', 'range-rover'];
+print(cars[1]);
+
+
