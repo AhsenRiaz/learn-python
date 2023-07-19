9eaab112c88138cf91952a32a75cb3896fa48a71
AhsenRiaz <ahsenriaz2001@gmail.com>
2023-07-19 21:56:28 +0500

    Create 03_set.py

---
+++ 03_set.py
@@ -0,0 +1,12 @@
+my_set = {1,2,3,4};
+print(my_set)
+
+
+# convert list to set
+cars = ['mercedes', 'totota'];
+my_set_to_list = set(cars);
+print(my_set_to_list)
+
+# set does not allow same multiple values
+my_set_1 = {1,2,1,3,3,1,2};
+print(my_set_1)
