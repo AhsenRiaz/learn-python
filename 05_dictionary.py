f92ca536c0baeec1796b23980fa5141b6ffa44d8
AhsenRiaz <ahsenriaz2001@gmail.com>
2023-07-19 21:56:46 +0500

    Create 05_dictionary.py

---
+++ 05_dictionary.py
@@ -0,0 +1,13 @@
+# A dictionary is a key value pair
+
+my_dictionary = {'name0':  "John", 'name1': "Bob"};
+
+print(my_dictionary);
+
+print(my_dictionary.get("name0"));
+print(my_dictionary["name0"], "\n") 
+
+print(my_dictionary.get("name1"));
+print(my_dictionary["name1"])
+
+
