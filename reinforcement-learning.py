19139e41fa3a28077bd4e4815de75790b0412530
M.AhsenRiaz <ahsenriaz2001@gmail.com>
2024-07-07 14:07:22 +0500

    Update reinforcement-learning.py

--- reinforcement-learning.py
+++ reinforcement-learning.py
@@ -1,81 +1,73 @@
-import gym
 import numpy as np
 import random
-from IPython.display import clear_output
-from time import sleep
+import time
+import gym
 
-# Creating thr env
-env = gym.make("Taxi-v2").env
-env.s = 328
-# Setting the number of iterations, penalties and reward to zero,
-epochs = 0
-penalties, reward = 0, 0
-frames = []
-done = False
-while not done:
-action = env.action_space.sample()
-state, reward, done, info = env.step(action)
-if reward == -10:
-penalties += 1
-# Put each rendered frame into the dictionary for animation
-frames.append({
-'frame': env.render(mode='ansi'),
-'state': state,
-'action': action,
-'reward': reward
-}
-)
-epochs += 1
-print("Timesteps taken: {}".format(epochs))
-print("Penalties incurred: {}".format(penalties))
-# Printing all the possible actions, states, rewards.
-def frames(frames):
-for i, frame in enumerate(frames):
-clear_output(wait=True)
-print(frame['frame'].getvalue())
-print(f"Timestep: {i + 1}")
-print(f"State: {frame['state']}")
-print(f"Action: {frame['action']}")
-print(f"Reward: {frame['reward']}")
-sleep(.1)
-frames(frames)
-
-
-# Init Taxi-V2 Env
-env = gym.make("Taxi-v2").env
-# Init arbitary values
-q_table = np.zeros([env.observation_space.n, env.action_space.n])
-# Hyperparameters
-alpha = 0.1
-gamma = 0.6
-epsilon = 0.1
-all_epochs = []
-all_penalties = []
-for i in range(1, 100001):
+env = gym.make("Taxi-v3")
 state = env.reset()
-# Init Vars
-epochs, penalties, reward, = 0, 0, 0
-done = False
+env.render()
+print("Current state is  :", state)
 
-while not done:
-if random.uniform(0, 1) < epsilon:
-# Check the action space
-action = env.action_space.sample()
-else:
-# Check the learned values
-action = np.argmax(q_table[state])
-next_state, reward, done, info = env.step(action)
-old_value = q_table[state, action]
-next_max = np.max(q_table[next_state])
-# Update the new value
-new_value = (1 - alpha) * old_value + alpha * \
-(reward + gamma * next_max)
-q_table[state, action] = new_value
-if reward == -10:
-penalties += 1
-state = next_state
-epochs += 1
-if i % 100 == 0:
-clear_output(wait=True)
-print("Episode: {i}")
-print("Training finished.")
+state_size = env.observation_space.n
+
+action_size = env.action_space.n
+
+q_table = np.zeros((state_size, action_size))
+episodes = 100000
+learning_rate = 0.1
+gamma = 0.7
+epsilon = 0.1
+
+def greedy_policy(state, table):
+    z = np.random.random()
+    if z > epsilon:
+        action = np.argmax(table[state])
+    else:
+        action = env.action_space.sample()
+    return action
+
+
+deltas = []
+for episode in range(1, episodes+1):
+    state = env.reset()
+    done = False
+    step = 0
+    change_t = 0
+    
+    if episode % 5000 == 0:
+        print("Episode: {}".format(episode))
+    while not done:
+        #env.render()
+        action = greedy_policy(state, q_table)
+        new_state, reward, done, info = env.step(action)
+        old_q = q_table[state, action]
+        
+        #Update
+        q_table[state, action] += learning_rate * (reward + gamma * np.max(q_table[new_state, :]) - q_table[state, action])
+        change_t = max(change_t, np.abs(q_table[state][action] - old_q))
+        state = new_state
+    deltas.append(change_t)
+    if deltas[-1] < 0.000000001:
+        break
+    episode += 1
+print("Maximum Difference is :", deltas[-1])
+
+state = env.reset()
+env.render()
+
+from IPython.display import clear_output
+import time
+
+done = False
+cumulative_reward = 0
+
+while(done==False):
+    best_action = np.argmax(q_table[state, :])
+    
+    state, reward, done, _ = env.step(best_action)
+    
+    cumulative_reward += reward
+    time.sleep(0.5)
+    clear_output(wait=True)
+    env.render()
+    print('Episode Reward :', cumulative_reward)
