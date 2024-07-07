325fb3c901579a20667be147f4475df390ac8405
M.AhsenRiaz <ahsenriaz2001@gmail.com>
2024-07-07 15:30:16 +0500

    Create star-search.py

---
+++ star-search.py
@@ -0,0 +1,23 @@
+def a_star_search(graph, start, goal):
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+    
+    while not frontier.empty():
+        current = frontier.get()
+        
+        if current == goal:
+            break
+        
+        for next_node in graph.neighbors(current):
+            new_cost = cost_so_far[current] + graph.cost(current, next_node)
+            if next_node not in cost_so_far or new_cost < cost_so_far[next_node]:
+                cost_so_far[next_node] = new_cost
+                priority = new_cost + heuristic(goal, next_node)
+                frontier.put(next_node, priority)
+                came_from[next_node] = current
+    
+    return reconstruct_path(came_from, start, goal)
